763f7cc4c7d2c254ff230f57b809a28b881f3b96
IYuriev <ilya.yuriev.s@gmail.com>
2024-03-02 14:28:36 +0200

    some fixes

--- Homework_04/task_01/point_05/main.js
+++ Homework_04/task_01/point_05/main.js
@@ -1,10 +1,19 @@
 "use strict"
 
-let start = +prompt('Введіть перше число');
+const start = +prompt('Введіть перше число');
 const finish = +prompt('Введіть друге число');
 
-for(let i = start; start <= finish; start++){
-    if(start % 5 === 0){
-        console.log(start);
+for(let i = start; i <= finish; i++){
+    if(i % 5 === 0){
+        console.log(i);
     }
+}
+
+let j = start;
+
+while(j <= finish){
+    if(j % 5 === 0){
+        console.log(j);
+    }
+    j++;
 }
--- Homework_04/task_01/point_06/main.js
+++ Homework_04/task_01/point_06/main.js
@@ -7,3 +7,12 @@ for(let i = 1; i <= 10; i++){
     result = number * i;
     console.log(`${number} помножити на ${i} дорівнює ${result}`);
 }
+
+let j = 1;
+let result2;
+
+while(j <= 10){
+    result2 = number * j;
+    console.log(`${number} помножити на ${j} дорівнює ${result2}`);
+    j++;
+}
